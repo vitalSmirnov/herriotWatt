fb2e3b62113be1b855d521bfc1df6142ea588b40
Smirnov Vitalii <temmets20@gmail.com>
2024-01-25 00:01:23 +0700

    img page

--- src/pages/imgPage/ui/ImgPage.tsx
+++ src/pages/imgPage/ui/ImgPage.tsx
@@ -6,9 +6,13 @@ import {IMaterial} from "../../../entities/models/IMaterial";
 import {ImgHeader} from "../../../components/ui/imgHeader/ui/ImgHeader";
 import {Drawer} from "../../../components/ui/Drawer/ui/Drawer";
 import {useEffect, useState} from "react";
+import {useAppDispatch, useAppSelector} from "../../../shared/helpers/hooks/redux";
+import {getPhotoSegment} from "../../../domain/repositories/api/segmentation/actionCreators/segmentationActionCreator";
 export const ImgPage = () => {
-
-    const [image, setImage] = useState(null);
+    const {projectId, imgId} = useParams()
+    const [image, setImage] = useState(File);
+    const dispatch = useAppDispatch();
+    // const {} = useAppSelector(state => state.segmentationReducer);
     const [isClear, setClear] = useState(false);
     const handleImageUploadCallback = (image: any) => {
         setImage(image)
@@ -18,30 +22,30 @@ export const ImgPage = () => {
         setClear(!isClear)
     }
 
-    const fetchedValues : IMaterial[] = [
-        {
-            id: '0',
-            description: 'description',
-            name: 'name',
-        },
-    ]
+    const savePicture = () => {
+        if (imgId) {
+            dispatch(getPhotoSegment({photoId: imgId, image: image}))
+        }
+    }
 
     useEffect(() => {
-
+        if (imgId !== 'new') {
+            dispatch(getPhotoSegment({photoId: imgId}))
+        }
     }, [image]);
 
     return(
         <>
             <div className={'img-page-main-container'}>
                 <div className={'card flex justify-content-between mt-5 w-100'}>
-                    <SearchDescription description={'Описание проекта'}/>
+                    <SearchDescription projectId={projectId} description={'Описание проекта'} name={'name'}/>
                     <div className={'w-full img-page-bordered'}>
                         <ImgHeader handleImageUploadCallback={handleImageUploadCallback} clearCanvasCallback={clearCanvasCallback}/>
                         {image && <Drawer isClear={isClear} image={image} clearCallback={clearCanvasCallback} />}
                     </div>
                 </div>
                 <div className={'w-full flex justify-content-center mb-5'}>
-                    <Button severity={'success'} label={'Сохранить изменения'}/>
+                    <Button onClick={savePicture} severity={'success'} label={'Сохранить изменения'}/>
                 </div>
             </div>
         </>
